58277f7cd8158f81fc0c5a9984829da1f7fb7fc5
Harsh Rathi <HarshRathi0553@gmail.com>
2020-06-16 10:54:43 -0700

    Another Commit

--- src/Renderer.java
+++ src/Renderer.java
@@ -1,15 +1,16 @@
 import javax.swing.*;
 import java.awt.*;
 
-public class Render extends JPanel {
-
-    private static final long serialVersComUID = 1L;
+public class Renderer extends JPanel {
 
     @Override
     protected void paintComponent(Graphics g){
+
         super.paintComponent(g);
+
         FlappyBird.flappyBird.repaint(g);
     }
 
 
 }
+
